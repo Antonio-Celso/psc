7637fd2a6ed4f5fdfbcc1a8800aee1a707b6fd0f
ismailmajjad <45630824+ismailmajjad@users.noreply.github.com>
2018-12-05 15:12:03 +0100

    Add files via upload

---
+++ src/VertexDist.java
@@ -0,0 +1,17 @@
+//package gps;
+
+//paire (sommet, distance) pour Dijkstra
+class VertexDist implements Comparable<VertexDist> {
+  final Vertex v;
+  final double d;
+
+  VertexDist(Vertex v, double d) {
+    this.v = v;
+    this.d = d;
+  }
+
+  public int compareTo(VertexDist that) {
+    double c = this.d - that.d;
+    return c < 0 ? -1 : c > 0 ? 1 : 0;
+  }
+}
